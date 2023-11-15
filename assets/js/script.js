bde39c22fb24926f19ccbac2d817ac5ffb4ec824
Igor Borges <borgesigor153@gmail.com>
2023-11-15 13:04:16 -0300

    :recycle: refactor: loop das imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,119 @@
+window.onload = function () {
+  exibirTodasImagens();
+};
 
-$(document).ready(function() {
+const PATH = "../../assets/img/";
 
-    $('.btn-filter').on('click', function() {
-        let tipo = $(this).attr('id');
-        //console.log(tipo);
-        let caixa = $('.box-img');
+var images = [
+  {
+    id: 1,
+    type: "web",
+    name: "Site da empresa Grupo Plantão",
+    category: "web",
+    link: "https://grupoplantao.srv.br/",
+    url: PATH + "web-1.png",
+  },
+  {
+    id: 2,
+    type: "jogo",
+    name: "Jogo da forca",
+    category: "jogos",
+    link: "https://igorios.github.io/jogo-da-memoria/",
+    url: PATH + "jogo-1.png",
+  },
+  {
+    id: 3,
+    type: "web",
+    name: "Site denuncia CRN5",
+    category: "web",
+    link: "https://denuncias.crn5.org.br/",
+    url: PATH + "web-2.png",
+  },
+  {
+    id: 4,
+    type: "jogo",
+    name: "Jogo da memoria",
+    category: "jogos",
+    link: "https://igorios.github.io/criptografia/",
+    url: PATH + "jogo-2.png",
+  },
 
-        $('.btn-port').removeClass('active');
-        $(this).addClass('active');
+  {
+    id: 5,
+    type: "pessoal",
+    name: "Lista de tarefas",
+    category: "pessoal",
+    link: "https://igorios.github.io/lista-tarefas/",
+    url: PATH + "pessoal-1.png",
+  },
+  {
+    id: 6,
+    type: "pessoal",
+    name: "Site para publicar eventos",
+    category: "pessoal",
+    link: "https://eventos-igorios.000webhostapp.com/",
+    url: PATH + "pessoal.png",
+  },
+];
 
-        if (tipo == 'btn-web') {
-            caixaEscolhida('web', caixa);
-        } else if (tipo == 'btn-jogo') {
-            caixaEscolhida('jogo', caixa);
-        } else if (tipo == "btn-pessoal") {
-            caixaEscolhida('pessoal', caixa);
-        } else {
-            caixaEscolhida('all', caixa);
-        }
+const btnTodos = document.querySelector("#btn-todo");
+const bntWeb = document.querySelector("#btn-web");
+const btnJogo = document.querySelector("#btn-jogo");
+const btnPessoal = document.querySelector("#btn-pessoal");
 
+const divContent = document.querySelector("#content-portfolio");
 
+function exibirTodasImagens() {
+  const imagesCopy = [...images];
+
+  divContent.innerHTML = "";
+
+  imagesCopy.splice(0, 6).forEach((todasImages) => {
+    divContent.innerHTML += `<a href="${todasImages.link}" target="_blank" class="col-md-4 box-img web">
+                            <img src="${todasImages.url}" class="img-fluid" alt="${todasImages.name}">
+                        </a>`;
+  });
+}
+btnTodos.addEventListener("click", exibirTodasImagens);
+
+btnPessoal.addEventListener("click", () => {
+  const todosPessoal = images.filter((image) => image.type == "pessoal");
+
+  divContent.innerHTML = "";
+
+  if (todosPessoal.length > 0) {
+    todosPessoal.forEach((pessoal) => {
+      divContent.innerHTML += `<a href="${pessoal.link}" target="_blank" class="col-md-4 box-img web">
+                <img src="${pessoal.url}" class="img-fluid" alt="${pessoal.name}">
+            </a>`;
     });
+  }
+});
 
+bntWeb.addEventListener("click", () => {
+  const todosWeb = images.filter((image) => image.type == "web");
 
-    function caixaEscolhida(tipo, caixa) {
+  divContent.innerHTML = "";
 
-        if (tipo == 'all') {
-            $(caixa).fadeIn();
-        } else {
-            $(caixa).each(function() {
-                if (!$(this).hasClass(tipo)) {
-                    $(this).fadeOut('show');
-                } else {
-                    $(this).fadeIn();
-                }
-            });
-        }
+  if (todosWeb.length > 0) {
+    todosWeb.forEach((web) => {
+      divContent.innerHTML += `<a href="${web.link}" target="_blank" class="col-md-4 box-img web">
+                <img src="${web.url}" class="img-fluid" alt="${web.name}">
+            </a>`;
+    });
+  }
+});
 
-    }
+btnJogo.addEventListener("click", () => {
+  const todosJogos = images.filter((image) => image.type == "jogo");
 
+  divContent.innerHTML = "";
 
+  if (todosJogos.length > 0) {
+    todosJogos.forEach((jogos) => {
+      divContent.innerHTML += `<a href="${jogos.link}" target="_blank" class="col-md-4 box-img web">
+                <img src="${jogos.url}" class="img-fluid" alt="${jogos.name}">
+            </a>`;
+    });
+  }
 });
