9d69ceb8e5378dca1050012b5e9fe1560bc76244
Igor Borges <borgesigor153@gmail.com>
2023-11-15 13:43:56 -0300

    :recycle: export images

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,59 +2,7 @@ window.onload = function () {
   exibirTodasImagens();
 };
 
-const PATH = "assets/img/";
-
-var images = [
-  {
-    id: 1,
-    type: "web",
-    name: "Site da empresa Grupo Plantão",
-    category: "web",
-    link: "https://grupoplantao.srv.br/",
-    url: PATH + "web-1.png",
-  },
-  {
-    id: 2,
-    type: "jogo",
-    name: "Jogo da forca",
-    category: "jogos",
-    link: "https://igorios.github.io/jogo-da-memoria/",
-    url: PATH + "jogo-1.png",
-  },
-  {
-    id: 3,
-    type: "web",
-    name: "Site denuncia CRN5",
-    category: "web",
-    link: "https://denuncias.crn5.org.br/",
-    url: PATH + "web-2.png",
-  },
-  {
-    id: 4,
-    type: "jogo",
-    name: "Jogo da memoria",
-    category: "jogos",
-    link: "https://igorios.github.io/criptografia/",
-    url: PATH + "jogo-2.png",
-  },
-
-  {
-    id: 5,
-    type: "pessoal",
-    name: "Lista de tarefas",
-    category: "pessoal",
-    link: "https://igorios.github.io/lista-tarefas/",
-    url: PATH + "pessoal-1.png",
-  },
-  {
-    id: 6,
-    type: "pessoal",
-    name: "Site para publicar eventos",
-    category: "pessoal",
-    link: "https://eventos-igorios.000webhostapp.com/",
-    url: PATH + "pessoal.png",
-  },
-];
+import { images } from "./images.js";
 
 const btnTodos = document.querySelector("#btn-todo");
 const bntWeb = document.querySelector("#btn-web");
